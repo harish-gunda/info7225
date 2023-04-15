cf68c1fa9c3c8b4fdb0afe0e69161d826a8f87a3
harish-gunda <gunda.sa@northeastern.edu>
2023-04-15 14:04:41 -0400

    adding elastic search and kafka

--- demo/rest_api_demo/views.py
+++ demo/rest_api_demo/views.py
@@ -6,6 +6,42 @@ from rest_framework import status
 from django.core.cache import cache
 import hashlib
 from rest_framework.permissions import IsAuthenticated
+from elasticsearch import Elasticsearch
+from kafka import KafkaProducer
+from kafka import KafkaConsumer
+import os
+
+# Single node via URL
+es = Elasticsearch(
+    "https://localhost:9200",
+    ca_certs="/Users/harish/Downloads/elasticsearch-8.7.0/config/certs/http_ca.crt",
+    basic_auth=("elastic", "A9sj3xj+v25dL2C1SDyu")
+)
+producer = KafkaProducer(bootstrap_servers='localhost:9092', api_version=(0,11,5))
+
+
+def index_helper(payload):
+    plan = payload
+    plan['plan_join'] = "plan"
+    es.index(index='plan_index', body=plan, id=plan['objectId'])
+
+    planCostShares = payload["planCostShares"]
+    planCostShares['plan_join'] = {"name": "planCostShares", "parent": plan['objectId']}
+    es.index(index='plan_index', body=planCostShares, id=planCostShares['objectId'], routing=plan['objectId'])
+    cache.set(planCostShares['objectId'], planCostShares)
+    linkedPlanServices = payload['linkedPlanServices']
+    for linkedPlanService in linkedPlanServices:
+        linkedService = linkedPlanService['linkedService']
+        planserviceCostShares = linkedPlanService['planserviceCostShares']
+        linkedPlanService['plan_join'] = {"name": "linkedPlanService", "parent": plan['objectId']}
+        es.index(index='plan_index', body=linkedPlanService, id=linkedPlanService['objectId'], routing=plan['objectId'])
+        cache.set(linkedPlanService['objectId'], linkedPlanService)
+        linkedService['plan_join'] = {"name": "linkedService", "parent": linkedPlanService['objectId']}
+        es.index(index='plan_index', body=linkedService, id=linkedService['objectId'], routing=linkedPlanService['objectId'])
+        cache.set(linkedService['objectId'], linkedService)
+        planserviceCostShares['plan_join'] = {"name": "planserviceCostShares", "parent": linkedPlanService['objectId']}
+        es.index(index='plan_index', body=planserviceCostShares, id=planserviceCostShares['objectId'], routing=linkedPlanService['objectId'])
+        cache.set(planserviceCostShares['objectId'], planserviceCostShares)
 
 
 # Plan create view
@@ -16,12 +52,25 @@ class PlanCreate(APIView):
         serializer = PlanSerializer(data=request.data)
         if serializer.is_valid():
             encoded = json.dumps(request.data, sort_keys=True).encode('utf-8')
-            print(encoded)
+            # print(encoded)
             md5_hash = hashlib.md5(encoded)
             if cache.get(serializer.data['objectId']):
                 return Response({"message": "An object exists with the given objectID"},
                                 status=status.HTTP_409_CONFLICT)
             cache.set(serializer.data['objectId'], serializer.data)
+            response = producer.send('FirstTopic', encoded)
+            # print(response)
+            producer.flush()
+            consumer = KafkaConsumer('FirstTopic', bootstrap_servers='localhost:9092', api_version=(0, 11, 5),
+                                     auto_offset_reset='earliest', group_id=None, consumer_timeout_ms=1000)
+            for msg in consumer:
+                payload = json.loads(msg.value)
+                # print(payload)
+                # es.index(index='contents', body=payload, id=serializer.data['objectId'])
+                # print('Done')
+                index_helper(payload)
+            # print('Done2')
+            consumer.close()
             return Response({"objectId": serializer.data['objectId']}, status=status.HTTP_201_CREATED,
                             headers={"Etag": md5_hash.hexdigest()})
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -50,6 +99,7 @@ class PlanRead(APIView):
             if md5_hash.hexdigest() == request.headers.get('if-none-match'):
                 if cache.get(id):
                     cache.delete(id)
+                    es.delete_by_query(index='plan_index', body={"query": {"match_all": {}}})
                     return Response(status=status.HTTP_204_NO_CONTENT)
                 return Response({"message": "Object Not found"}, status=status.HTTP_404_NOT_FOUND)
             else:
@@ -71,7 +121,10 @@ class PlanRead(APIView):
                     print(encoded)
                     md5_hash = hashlib.md5(encoded)
                     if cache.get(serializer.data['objectId']):
+                        cache.clear()
                         cache.set(serializer.data['objectId'], serializer.data)
+                        es.delete_by_query(index='plan_index', body={"query": {"match_all": {}}})
+                        index_helper(serializer.data)
                         return Response({"message": "resource updated successfully"}, status=status.HTTP_200_OK,
                                         headers={"Etag": md5_hash.hexdigest()})
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -107,11 +160,14 @@ class PlanRead(APIView):
                     md5_hash = hashlib.md5(encoded)
                     if cache.get(serializer.data['objectId']):
                         cache.set(serializer.data['objectId'], plan)
+                        index_helper(plan)
                         return Response({"message": "resource updated successfully"}, status=status.HTTP_200_OK,
                                         headers={"Etag": md5_hash.hexdigest()})
                 return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
             else:
                 return Response({"message": "Invalid etag"}, status=status.HTTP_412_PRECONDITION_FAILED)
         return Response({"message": "please provide etag"}, status=status.HTTP_400_BAD_REQUEST)
+
+
 
 
